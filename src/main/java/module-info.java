2118605adf0920574d1d3fb7d81617ae1cacf33c
Chani17 <cucute8878@naver.com>
2024-06-03 21:59:30 +0900

    add: oracle database jdbc module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module kosa.watermelon.watermelonmusic {
     requires javafx.fxml;
     requires java.sql;
     requires javafx.media;
+    requires com.oracle.database.jdbc;
 
     opens kosa.watermelon.watermelonmusic to javafx.fxml;
     exports kosa.watermelon.watermelonmusic;
