81c433168b115d8ddc3cca1062c6dc2c2c26f7ea
Kim-Hyo-Jeong <lk7938@naver.com>
2024-06-05 18:39:20 +0900

    Remove: Remove Unused import
    사용되지 않은 import 제거

--- src/main/java/kosa/watermelon/watermelonmusic/LoginController.java
+++ src/main/java/kosa/watermelon/watermelonmusic/LoginController.java
@@ -6,8 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -20,13 +18,11 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ByteArrayInputStream;
 import java.net.URL;
-import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
--- src/main/java/kosa/watermelon/watermelonmusic/TestDB_mypage.java
+++ src/main/java/kosa/watermelon/watermelonmusic/TestDB_mypage.java
@@ -2,7 +2,6 @@ package kosa.watermelon.watermelonmusic;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Blob;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
