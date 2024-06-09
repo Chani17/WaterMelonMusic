7c09ff69e7d39d425b73e49003e5e296d8e9d7d4
Kim-Hyo-Jeong <lk7938@naver.com>
2024-06-10 01:00:09 +0900

    style: Add Logo to Application
    애플리케이션 실행 시 로고 지정

--- src/main/java/kosa/watermelon/watermelonmusic/MainApplication.java
+++ src/main/java/kosa/watermelon/watermelonmusic/MainApplication.java
@@ -3,6 +3,7 @@ package kosa.watermelon.watermelonmusic;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
@@ -12,6 +13,10 @@ public class MainApplication extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 464);
         stage.setTitle("WaterMelon Music!");
+        Image icon = new Image(
+        		getClass().getResourceAsStream("/kosa/watermelon/watermelonmusic/watermelon_logo_only.png")); // 로고 이미지 파일 경로 지정
+        stage.getIcons().add(icon);
+        
         stage.setScene(scene);
         //stage.setResizable(false); // 창 크기 조절 비활성화
         stage.show();
