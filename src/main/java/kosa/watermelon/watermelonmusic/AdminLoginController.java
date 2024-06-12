13a64d02c625d0507849c4f98df3b670aa9a9ddf
Kim-Hyo-Jeong <lk7938@naver.com>
2024-06-12 17:57:43 +0900

    add: comment
    AdminLoginController.java 주석 설정

--- src/main/java/kosa/watermelon/watermelonmusic/AdminLoginController.java
+++ src/main/java/kosa/watermelon/watermelonmusic/AdminLoginController.java
@@ -18,112 +18,133 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 
+/**
+ * AdminLoginController : 관리자 로그인 처리
+ */
 public class AdminLoginController {
-    @FXML
-    private TextField adminID;
+	@FXML private TextField adminID;
+	@FXML private PasswordField adminPW;
+	@FXML private TextField adminEmail;
 
-    @FXML
-    private PasswordField adminPW;
+	private Stage loginControllerStage;
 
-    @FXML
-    private TextField adminEmail;
+	/**
+	 * 로그인 컨트롤러의 Stage 설정
+	 * 
+	 * @param stage 현재 로그인 컨트롤러의 Stage
+	 */
+	public void setLoginControllerStage(Stage stage) {
+		this.loginControllerStage = stage;
+	}
 
-    private Stage loginControllerStage;
-    
-    public void setLoginControllerStage(Stage stage) {
-        this.loginControllerStage = stage;
-    }
-    
-    @FXML
-    private void handleAdminLogin() {
-        String id = adminID.getText();
-        String pw = adminPW.getText();
-        String email = adminEmail.getText();
+	/**
+	 * 관리자 로그인 처리 메서드 관리자 인증에 성공하면 관리자 페이지로 이동
+	 */
+	@FXML
+	private void handleAdminLogin() {
+		String id = adminID.getText();
+		String pw = adminPW.getText();
+		String email = adminEmail.getText();
 
-        // Admin credentials check
-        if (validateAdminCredentials(id, pw, email)) {
-        	// Admin 로그인 정보를 세션에 설정
-        	Member adminMember = getAdminMember(id);
-        	SessionManager.getInstance().setCurrentMember(adminMember);
-            
-        	try {
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("adminPage.fxml"));
-                Parent root = loader.load();
-                Stage stage = new Stage();
-                stage.setTitle("관리자 페이지");
-                stage.setScene(new Scene(root));
-                Image icon = new Image(
-    	        		getClass().getResourceAsStream("/kosa/watermelon/watermelonmusic/watermelon_logo_only.png")); // 로고 이미지 파일 경로 지정
-    			stage.getIcons().add(icon);
-                stage.show();
-                
-                adminID.getScene().getWindow().hide();
-                
-                if (loginControllerStage != null) {
-                	loginControllerStage.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Alert alert = new Alert(AlertType.ERROR);
-            alert.setTitle("로그인 실패");
-            alert.setHeaderText(null);
-            alert.setContentText("관리자 권한이 없습니다.");
-            alert.showAndWait();
-        }
-    }
+		// 관리자 확인
+		if (validateAdminCredentials(id, pw, email)) {
+			// admin 로그인 정보를 세션에 설정
+			Member adminMember = getAdminMember(id);
+			SessionManager.getInstance().setCurrentMember(adminMember);
 
-    private boolean validateAdminCredentials(String id, String pw, String email) {
-    	if (!"admin".equals(id)) {
-    		return false;
-    	}
-    	
-    	String query = "SELECT MEMBER_ID FROM MEMBER WHERE MEMBER_ID = ? AND MEMBER_PW = ? AND EMAIL = ?";
-        try (Connection conn = DBUtil.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(query)) {
-            pstmt.setString(1, id);
-            pstmt.setString(2, pw);
-            pstmt.setString(3, email);
-            ResultSet rs = pstmt.executeQuery();
-            return rs.next();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-    
-    private Member getAdminMember(String id) {
-        Member member = null;
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("adminPage.fxml"));
+				Parent root = loader.load();
+				Stage stage = new Stage();
+				stage.setTitle("관리자 페이지");
+				stage.setScene(new Scene(root));
 
-        String query = "SELECT MEMBER_ID, MEMBER_PW, EMAIL, NICKNAME, PROFILE_IMAGE, GENDER, BIRTH FROM MEMBER WHERE MEMBER_ID = ?";
-        try (Connection connection = DBUtil.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-            preparedStatement.setString(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
+				// 로고 이미지 파일 경로 지정
+				Image icon = new Image(
+						getClass().getResourceAsStream("/kosa/watermelon/watermelonmusic/watermelon_logo_only.png"));
+				stage.getIcons().add(icon);
+				stage.show();
 
-            if (resultSet.next()) {
-                String memberId = resultSet.getString("MEMBER_ID");
-                String memberPw = resultSet.getString("MEMBER_PW");
-                String email = resultSet.getString("EMAIL");
-                String nickname = resultSet.getString("NICKNAME");
-                byte[] profileImage = null;
-                
-                BFILE bfile = ((OracleResultSet) resultSet).getBFILE("PROFILE_IMAGE");
-                if (bfile != null) {
-                    bfile.open();
-                    profileImage = bfile.getBytes();
-                    bfile.close();
-                }
+				adminID.getScene().getWindow().hide();
 
-                String gender = resultSet.getString("GENDER");
-                java.sql.Date birth = resultSet.getDate("BIRTH");
-                member = new Member(memberId, memberPw, email, nickname, profileImage, gender, birth.toLocalDate());
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+				if (loginControllerStage != null) {
+					loginControllerStage.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("로그인 실패");
+			alert.setHeaderText(null);
+			alert.setContentText("관리자 권한이 없습니다.");
+			alert.showAndWait();
+		}
+	}
 
-        return member;
-    }
+	/**
+	 * 관리자 자격 증명을 검증하는 메서드
+	 * 
+	 * @param id    관리자 ID
+	 * @param pw    관리자 PW
+	 * @param email 관리자 이메일
+	 * @return 관리자 자격이 있는 경우 true, 자격이 없다면 false
+	 */
+	private boolean validateAdminCredentials(String id, String pw, String email) {
+		if (!"admin".equals(id)) {
+			return false;
+		}
+
+		String query = "SELECT MEMBER_ID FROM MEMBER WHERE MEMBER_ID = ? AND MEMBER_PW = ? AND EMAIL = ?";
+		try (Connection conn = DBUtil.getConnection(); PreparedStatement pstmt = conn.prepareStatement(query)) {
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);
+			pstmt.setString(3, email);
+			ResultSet rs = pstmt.executeQuery();
+			return rs.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * 관리자 계정의 Member 객체를 반환하는 메서드
+	 * 
+	 * @param id 관리자 ID
+	 * @return 해당 ID의 Member 객체
+	 */
+	private Member getAdminMember(String id) {
+		Member member = null;
+
+		String query = "SELECT MEMBER_ID, MEMBER_PW, EMAIL, NICKNAME, PROFILE_IMAGE, GENDER, BIRTH FROM MEMBER WHERE MEMBER_ID = ?";
+		try (Connection connection = DBUtil.getConnection();
+				PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+			preparedStatement.setString(1, id);
+			ResultSet resultSet = preparedStatement.executeQuery();
+
+			if (resultSet.next()) {
+				String memberId = resultSet.getString("MEMBER_ID");
+				String memberPw = resultSet.getString("MEMBER_PW");
+				String email = resultSet.getString("EMAIL");
+				String nickname = resultSet.getString("NICKNAME");
+				byte[] profileImage = null;
+
+				BFILE bfile = ((OracleResultSet) resultSet).getBFILE("PROFILE_IMAGE");
+				if (bfile != null) {
+					bfile.open();
+					profileImage = bfile.getBytes();
+					bfile.close();
+				}
+
+				String gender = resultSet.getString("GENDER");
+				java.sql.Date birth = resultSet.getDate("BIRTH");
+				member = new Member(memberId, memberPw, email, nickname, profileImage, gender, birth.toLocalDate());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return member;
+	}
 }
